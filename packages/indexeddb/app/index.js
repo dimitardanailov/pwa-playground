4c6e951ac81b2b60ce1e3d4802d5b4ed4783d2dc
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-10-03 15:16:18 +0300

    Application is able to get text field value.

--- packages/indexeddb/app/index.js
+++ packages/indexeddb/app/index.js
@@ -1,4 +1,9 @@
 window.addEventListener('load', async () => {
   const titleTextField = document.getElementById('title');
   const articleSubmitButton = document.getElementById('articleSubmitButton');
+
+  articleSubmitButton.addEventListener('click', async e => {
+    e.preventDefault();
+    const article = titleTextField.value;
+  });
 });

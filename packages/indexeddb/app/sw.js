a8a0d5c5d053dc7ddb08b01a6a3e17f4cf0de961
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-10-04 13:08:44 +0300

    Service worker is able to access IndexedDB.

--- packages/indexeddb/app/sw.js
+++ packages/indexeddb/app/sw.js
@@ -1,6 +1,57 @@
 /* eslint-disable no-restricted-globals */
 console.log('Service worker -> IndexedDB playground waking up!');
 
+const DB_CONFIG = {
+  name: 'Articles',
+  version: 1,
+  storeName: 'articles',
+};
+
+/**
+ * Source and idea:
+ * https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
+ */
+function openDB() {
+  return self.indexedDB.open(DB_CONFIG.name, DB_CONFIG.version);
+}
+
+async function initDB() {
+  const promise = new Promise((resolve, reject) => {
+    const request = openDB();
+
+    request.onerror = e => {
+      console.error('IndexedDB error', request.error);
+      reject(e);
+    };
+
+    request.onsuccess = e => {
+      resolve(e.target.result);
+    };
+  });
+
+  return promise;
+}
+
+async function getArticleStore(mode) {
+  const db = await initDB();
+  const tx = db.transaction(DB_CONFIG.storeName, mode);
+  const store = tx.objectStore(DB_CONFIG.storeName);
+
+  return store;
+}
+
+async function findArticleByTitle(title) {
+  const store = await getArticleStore();
+
+  const promise = new Promise(resolve => {
+    store.get(title).onsuccess = e => {
+      resolve(e.target.result);
+    };
+  });
+
+  return promise;
+}
+
 self.addEventListener('install', () => {
   console.log('Service Worker installed.');
 });
@@ -9,7 +60,8 @@ self.addEventListener('activate', () => {
   console.log('Service Worker activating.');
 });
 
-self.addEventListener('message', e => {
-  console.log(`SW Received Message: ${e.data}`);
-  console.log(e);
+self.addEventListener('message', async e => {
+  if (self.indexedDB) {
+    const records = await findArticleByTitle(e.data);
+  }
 });

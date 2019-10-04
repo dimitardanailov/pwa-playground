3439839a658b7ea7dd6b9ae4c6eefe426f1b07ef
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-10-04 14:40:08 +0300

    Application adds service worker like a promise.

--- packages/indexeddb/app/index.js
+++ packages/indexeddb/app/index.js
@@ -4,6 +4,16 @@ import {
   findArticlesByIndex,
 } from './IndexedDB/article-store';
 
+async function registerServiceWorker() {
+  const promise = new Promise(resolve => {
+    navigator.serviceWorker.register('/sw.js').then(swReg => {
+      resolve(swReg);
+    });
+  });
+
+  return promise;
+}
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
     // Init db
@@ -12,21 +22,20 @@ if ('serviceWorker' in navigator) {
     const titleTextField = document.getElementById('title');
     const articleSubmitButton = document.getElementById('articleSubmitButton');
 
-    navigator.serviceWorker.register('/sw.js').then(swReg => {
-      console.log('Service Worker is registered', swReg);
+    const worker = await registerServiceWorker();
+    console.log('Service Worker is registered', worker);
 
-      articleSubmitButton.addEventListener('click', async e => {
-        e.preventDefault();
-        const article = titleTextField.value;
+    articleSubmitButton.addEventListener('click', async e => {
+      e.preventDefault();
+      const article = titleTextField.value;
 
-        const dbArticle = await findArticleByTitle(db, article);
-        if (dbArticle === null) {
-          navigator.serviceWorker.controller.postMessage(article);
-        }
+      const dbArticle = await findArticleByTitle(db, article);
+      if (dbArticle === null) {
+        navigator.serviceWorker.controller.postMessage(article);
+      }
 
-        const dbArticles = await findArticlesByIndex(db, 'title', article);
-        console.log('articles', dbArticles);
-      });
+      const dbArticles = await findArticlesByIndex(db, 'title', article);
+      console.log('articles', dbArticles);
     });
   });
 }
